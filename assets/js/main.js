1cce0a40759ae346b4f9d0b564aaf5ac3c60faf7
JV Freitas <134442764+jvfreitas61@users.noreply.github.com>
2024-08-01 19:22:01 -0300

    novo-repositorio

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+function clickMenu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    }
+}
+
+var tabela1 = document.querySelector('#tabela-1')
+var tabela2 = document.querySelector('#tabela-2')
+var tabela3 = document.querySelector('#tabela-3')
+var titleGuia1 = document.querySelector('#title-guia1')
+var titleGuia2 = document.querySelector('#title-guia2')
+
+function clickVoltar(){
+    if(tabela2.style.display = 'block'){
+        tabela2.style.display = 'none'
+        titleGuia2.style.display = 'none'
+        titleGuia1.style.display = 'block'
+        tabela1.style.display = 'block'
+    }
+}
+
+function clickAvancar(){
+    if(tabela1.style.display = 'block'){
+        tabela1.style.display = 'none'
+        titleGuia1.style.display = 'none'
+        titleGuia2.style.display = 'block'
+        tabela2.style.display = 'block'
+    }
+}
