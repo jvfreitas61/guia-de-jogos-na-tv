4c27fa3af7233a2e74f937f6335f30847183e3e6
JV Freitas <134442764+jvfreitas61@users.noreply.github.com>
2024-08-16 01:23:46 -0300

    melhoria-listas

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,13 @@ function clickMenu(){
     }
 }
 
-var tabela1 = document.querySelector('#tabela-1')
+/*var tabela1 = document.querySelector('#tabela-1')
 var tabela2 = document.querySelector('#tabela-2')
 var tabela3 = document.querySelector('#tabela-3')
 var titleGuia1 = document.querySelector('#title-guia1')
-var titleGuia2 = document.querySelector('#title-guia2')
+var titleGuia2 = document.querySelector('#title-guia2')*/
 
-function clickVoltar(){
+/*function clickVoltar(){
     if(tabela2.style.display = 'block'){
         tabela2.style.display = 'none'
         titleGuia2.style.display = 'none'
@@ -28,21 +28,43 @@ function clickAvancar(){
         titleGuia2.style.display = 'block'
         tabela2.style.display = 'block'
     }
-}
+}*/
 
 const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
 let listaAtual = 0;
-const listas = document.querySelectorAll(".cartao");
+
+const listas = document.querySelectorAll(".tabela");
+const titulos = document.querySelectorAll(".titulo-guia");
 
 btnAvancar.addEventListener("click", function(){
-    if (cartaoAtual === cartoes.length - 1) return;
-    const cartaoSelecionado = document.querySelector("selecionado");
-    cartaoSelecionado.classList.remove("selecionado");
-
-    
-    cartaoAtual++;
-    cartoes[cartaoAtual].classList.add("selecionado");
+    if (listaAtual === listas.length - 1) return;
+    //const listaSelecionado = document.querySelector(".selecionado");
+    //listaSelecionado.classList.remove("selecionado");
+    removerLista();
+    listaAtual++;
+    listas[listaAtual].classList.add("selecionado");
+    titulos[listaAtual].classList.add("titulo-selecionado");
 });
+
+
+
+btnVoltar.addEventListener("click", function(){
+    if (listaAtual === 0) return;
+    //const listaSelecionado = document.querySelector(".selecionado");
+    //listaSelecionado.classList.remove("selecionado");
+    removerLista();
+    listaAtual--;
+    listas[listaAtual].classList.add("selecionado");
+    titulos[listaAtual].classList.add("titulo-selecionado");
+});
+
+function removerLista(){
+    const listaSelecionado = document.querySelector(".selecionado");
+    listaSelecionado.classList.remove("selecionado");
+    const tituloSelecionado = document.querySelector(".titulo-selecionado");
+    tituloSelecionado.classList.remove("titulo-selecionado");
+}
 
 /*var jogo = document.querySelectorAll('.jogo');
 jogo.addEventListener('click', adicionaClasse());
