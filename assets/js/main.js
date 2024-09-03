3f697bd3555a65b805708eafcf00b517a24bdc41
JV Freitas <134442764+jvfreitas61@users.noreply.github.com>
2024-09-03 01:31:08 -0300

    teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,30 +6,6 @@ function clickMenu(){
     }
 }
 
-/*var tabela1 = document.querySelector('#tabela-1')
-var tabela2 = document.querySelector('#tabela-2')
-var tabela3 = document.querySelector('#tabela-3')
-var titleGuia1 = document.querySelector('#title-guia1')
-var titleGuia2 = document.querySelector('#title-guia2')*/
-
-/*function clickVoltar(){
-    if(tabela2.style.display = 'block'){
-        tabela2.style.display = 'none'
-        titleGuia2.style.display = 'none'
-        titleGuia1.style.display = 'block'
-        tabela1.style.display = 'block'
-    }
-}
-
-function clickAvancar(){
-    if(tabela1.style.display = 'block'){
-        tabela1.style.display = 'none'
-        titleGuia1.style.display = 'none'
-        titleGuia2.style.display = 'block'
-        tabela2.style.display = 'block'
-    }
-}*/
-
 const btnAvancar = document.getElementById("btn-avancar");
 const btnVoltar = document.getElementById("btn-voltar");
 let listaAtual = 0;
@@ -279,6 +255,9 @@ function carregar(){
                 const transmissao = document.createElement("ul")
                 divJogo.appendChild(transmissao);
                 transmissao.classList.add("transmissao")
+                    
+                    console.log(jogo.transmissao);
+                    
 
             })
         })
