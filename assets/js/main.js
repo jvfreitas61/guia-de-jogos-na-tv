9d48c247779390ec073b6f48d8e2ba1f57a64321
JV Freitas <134442764+jvfreitas61@users.noreply.github.com>
2024-09-03 03:13:02 -0300

    json-funcionando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-function clickMenu(){
-    if(itens.style.display == 'block'){
+function clickMenu() {
+    if (itens.style.display == 'block') {
         itens.style.display = 'none'
-    } else{
+    } else {
         itens.style.display = 'block'
     }
 }
@@ -13,7 +13,7 @@ let listaAtual = 0;
 const listas = document.querySelectorAll(".tabela");
 const titulos = document.querySelectorAll(".titulo-guia");
 
-btnAvancar.addEventListener("click", function(){
+btnAvancar.addEventListener("click", function () {
     if (listaAtual === listas.length - 1) return;
     //const listaSelecionado = document.querySelector(".selecionado");
     //listaSelecionado.classList.remove("selecionado");
@@ -25,7 +25,7 @@ btnAvancar.addEventListener("click", function(){
 
 
 
-btnVoltar.addEventListener("click", function(){
+btnVoltar.addEventListener("click", function () {
     if (listaAtual === 0) return;
     //const listaSelecionado = document.querySelector(".selecionado");
     //listaSelecionado.classList.remove("selecionado");
@@ -35,7 +35,7 @@ btnVoltar.addEventListener("click", function(){
     titulos[listaAtual].classList.add("titulo-selecionado");
 });
 
-function removerLista(){
+function removerLista() {
     const listaSelecionado = document.querySelector(".selecionado");
     listaSelecionado.classList.remove("selecionado");
     const tituloSelecionado = document.querySelector(".titulo-selecionado");
@@ -54,17 +54,19 @@ function abrirJogo(){
 }*/
 
 const listaJogos = document.querySelectorAll('.jogo');
-//console.log(jogo)
+console.log(listaJogos)
 
 listaJogos.forEach((jogo) => {
     jogo.addEventListener('click', callBack)
-    
+
 })
 
 
 
 
-function callBack(event){
+
+
+function callBack(event) {
 
     console.log(event.currentTarget);
     const tituloJogo = event.currentTarget.querySelector('.estadio').innerText;
@@ -80,7 +82,7 @@ function callBack(event){
 
     const visitante = event.currentTarget.querySelector('.time-visitante');
     //console.log(visitante);
-    
+
     const nomeMandante = mandante.innerText;
     const nomeVisitante = visitante.innerText;
 
@@ -93,15 +95,15 @@ function callBack(event){
 
 
     const logoMandante = mandante.querySelector('img.logo-escudo').getAttribute('src');
-    
+
     //console.log(logoMandante);
-    
+
     const logoVisitante = visitante.querySelector('img.logo-escudo').getAttribute('src');
-    
+
     //console.log(logoVisitante);
 
 
-   
+
 
 
     const tituloJogoAlt = document.querySelector('.estadio-alt');
@@ -110,12 +112,12 @@ function callBack(event){
     dataJogoAlt.innerHTML = dataJogo;
     const horarioJogoAlt = document.querySelector('.horario-jogo-alt');
     horarioJogoAlt.innerHTML = horarioJogo;
-    
+
     const mandanteAlt = document.querySelector('.nome-time-alt-m');
     const visitanteAlt = document.querySelector('.nome-time-alt-v');
     mandanteAlt.innerHTML = nomeMandante;
     visitanteAlt.innerHTML = nomeVisitante;
-    
+
     const logoMandanteAlt = document.querySelector('img.logo-escudo-alt-m');
     logoMandanteAlt.setAttribute('src', logoMandante);
     const logoVisitanteAlt = document.querySelector('img.logo-escudo-alt-v');
@@ -125,25 +127,25 @@ function callBack(event){
     const listaTransmissao = event.currentTarget.querySelectorAll('li');
     console.log(listaTransmissao);
     const listaTransmissaoAlt = document.querySelector('.transmissao-alt');
-    
+
     listaTransmissao.forEach((item) => {
-        
-        
-        
+
+
+
         //cloneLi.classList.add('logo-transmissão-alt');
         //cloneLi.children[1].classList.remove('.logo-transmissão')
         console.log(item);
         //console.log(item.children[0]);
         const cloneLi = item.cloneNode(true);
         listaTransmissaoAlt.appendChild(cloneLi);
-        
+
         const listaImg = listaTransmissaoAlt.querySelector('.logo-transmissao');
         listaImg.classList.remove('logo-transmissao');
         listaImg.classList.add('logo-transmissao-alt');
         console.log(listaTransmissaoAlt);
     })
-    
-    
+
+
 
 }
 
@@ -152,13 +154,13 @@ linksExternos.forEach((link) => {
     link.addEventListener('click', handleLink);
 })
 
-function handleLink(event){
+function handleLink(event) {
     event.preventDefault();
-    
+
 }
 
 //const tituloJogo = document.querySelector('.estadio');
-    //console.log(tituloJogo);
+//console.log(tituloJogo);
 
 
 /*
@@ -173,19 +175,19 @@ function callbackLista(event){
 //const btnFecharJogo = document.querySelector('#fechar-jogo');
 //const jogoAlt = document.getElementById('jogo-alt');
 
-function fecharJogo(){
-    if(jogoAlt.style.display == 'block'){
+function fecharJogo() {
+    if (jogoAlt.style.display == 'block') {
         jogoAlt.style.display = 'none'
     }
 }
 
-function carregar(){
+function jogoHoje() {
     fetch('assets/js/jogos.json')
         .then(response => response.json())
         .then(jogos => {
-            const listaJogos = document.querySelector('#listaJogos');
-            console.log(listaJogos);
-            jogos.map(jogo=>{
+            const listaJogos = document.querySelector('#jogosHoje');
+            //console.log(listaJogos);
+            jogos.map(jogo => {
                 const card = document.createElement("li");
                 listaJogos.appendChild(card);
                 const divJogo = document.createElement("div");
@@ -194,73 +196,178 @@ function carregar(){
                 const jogoHeading = document.createElement("div")
                 divJogo.appendChild(jogoHeading);
                 jogoHeading.classList.add("jogo-heading");
-                    const dataJogo = document.createElement("p")
-                    jogoHeading.appendChild(dataJogo);
-                    dataJogo.classList.add("data-jogo")
-                    dataJogo.textContent = jogo.dataJogo
+                const dataJogo = document.createElement("p")
+                jogoHeading.appendChild(dataJogo);
+                dataJogo.classList.add("data-jogo")
+                dataJogo.textContent = jogo.dataJogo
 
-                    const estadio = document.createElement("p")
-                    jogoHeading.appendChild(estadio);
-                    estadio.classList.add("estadio")
-                    estadio.textContent = jogo.estadio
+                const estadio = document.createElement("p")
+                jogoHeading.appendChild(estadio);
+                estadio.classList.add("estadio")
+                estadio.textContent = jogo.estadio
 
-                    const horaJogo = document.createElement("p")
-                    jogoHeading.appendChild(horaJogo);
-                    horaJogo.classList.add("horario-jogo")
-                    horaJogo.textContent = jogo.horaJogo
+                const horaJogo = document.createElement("p")
+                jogoHeading.appendChild(horaJogo);
+                horaJogo.classList.add("horario-jogo")
+                horaJogo.textContent = jogo.horaJogo
 
                 const confronto = document.createElement("div");
                 divJogo.appendChild(confronto);
                 confronto.classList.add("confronto")
-                    const mandante = document.createElement("div")
-                    confronto.appendChild(mandante);
-                    mandante.classList.add("time-mandante")
-                        const nomeMandante = document.createElement("span")
-                        mandante.appendChild(nomeMandante)
-                        nomeMandante.classList.add("nome-time")
-                        nomeMandante.textContent = jogo.nomeMandante
+                const mandante = document.createElement("div")
+                confronto.appendChild(mandante);
+                mandante.classList.add("time-mandante")
+                const nomeMandante = document.createElement("span")
+                mandante.appendChild(nomeMandante)
+                nomeMandante.classList.add("nome-time")
+                nomeMandante.textContent = jogo.mandante.nome
 
-                        const logoMandante = document.createElement("span")
-                        mandante.appendChild(logoMandante)
-                        
-                        const imgMandante = document.createElement("img")
-                        logoMandante.appendChild(imgMandante)
-                        imgMandante.classList.add("logo-escudo")
-                        imgMandante.src = jogo.imgMandante
-                        imgMandante.alt = jogo.nomeMandante
+                const logoMandante = document.createElement("span")
+                mandante.appendChild(logoMandante)
 
-                    const versus = document.createElement("span")
-                    confronto.appendChild(versus);
-                    versus.classList.add("versus")
-                    versus.textContent = "X"
+                const imgMandante = document.createElement("img")
+                logoMandante.appendChild(imgMandante)
+                imgMandante.classList.add("logo-escudo")
+                imgMandante.src = jogo.mandante.escudo
+                imgMandante.alt = jogo.mandante.nome
 
-                    const visitante = document.createElement("div")
-                    confronto.appendChild(visitante);
-                    visitante.classList.add("time-visitante")
-                        
-                        const logoVisitante = document.createElement("span")
-                        visitante.appendChild(logoVisitante)
-                        
-                        const imgVisitante = document.createElement("img")
-                        logoVisitante.appendChild(imgVisitante)
-                        imgVisitante.classList.add("logo-escudo")
-                        imgVisitante.src = jogo.imgVisitante
-                        imgVisitante.alt = jogo.nomeVisitante
+                const versus = document.createElement("span")
+                confronto.appendChild(versus);
+                versus.classList.add("versus")
+                versus.textContent = "X"
 
-                        const nomeVisitante = document.createElement("span")
-                        visitante.appendChild(nomeVisitante)
-                        nomeVisitante.classList.add("nome-time")
-                        nomeVisitante.textContent = jogo.nomeVisitante
+                const visitante = document.createElement("div")
+                confronto.appendChild(visitante);
+                visitante.classList.add("time-visitante")
+
+                const logoVisitante = document.createElement("span")
+                visitante.appendChild(logoVisitante)
+
+                const imgVisitante = document.createElement("img")
+                logoVisitante.appendChild(imgVisitante)
+                imgVisitante.classList.add("logo-escudo")
+                imgVisitante.src = jogo.visitante.escudo
+                imgVisitante.alt = jogo.visitante.nome
+
+                const nomeVisitante = document.createElement("span")
+                visitante.appendChild(nomeVisitante)
+                nomeVisitante.classList.add("nome-time")
+                nomeVisitante.textContent = jogo.visitante.nome
 
                 const transmissao = document.createElement("ul")
                 divJogo.appendChild(transmissao);
                 transmissao.classList.add("transmissao")
-                    
-                    console.log(jogo.transmissao);
-                    
-
+                for (var i = 0; i < jogo.transmissao.length; i++) {
+                    const canal = document.createElement("li")
+                    transmissao.appendChild(canal);
+                    const linkCanal = document.createElement("a")
+                    canal.appendChild(linkCanal)
+                    linkCanal.href = jogo.transmissao[i].link
+                    const logoCanal = document.createElement("img")
+                    linkCanal.appendChild(logoCanal)
+                    logoCanal.classList.add("logo-transmissao")
+                    logoCanal.src = jogo.transmissao[i].logoCanal
+                    logoCanal.alt = jogo.transmissao[i].nomeCanal
+                    //linkCanal.preventDefault();
+                }
             })
         })
 }
 
-carregar();
+jogoHoje();
+
+
+
+function jogoAmanha() {
+    fetch('assets/js/jogos.json')
+        .then(response => response.json())
+        .then(jogos => {
+            const listaJogos = document.querySelector('#jogosAmanha');
+            //console.log(listaJogos);
+            jogos.map(jogo => {
+                const card = document.createElement("li");
+                listaJogos.appendChild(card);
+                const divJogo = document.createElement("div");
+                card.appendChild(divJogo);
+                divJogo.classList.add("jogo");
+                const jogoHeading = document.createElement("div")
+                divJogo.appendChild(jogoHeading);
+                jogoHeading.classList.add("jogo-heading");
+                const dataJogo = document.createElement("p")
+                jogoHeading.appendChild(dataJogo);
+                dataJogo.classList.add("data-jogo")
+                dataJogo.textContent = jogo.dataJogo
+
+                const estadio = document.createElement("p")
+                jogoHeading.appendChild(estadio);
+                estadio.classList.add("estadio")
+                estadio.textContent = jogo.estadio
+
+                const horaJogo = document.createElement("p")
+                jogoHeading.appendChild(horaJogo);
+                horaJogo.classList.add("horario-jogo")
+                horaJogo.textContent = jogo.horaJogo
+
+                const confronto = document.createElement("div");
+                divJogo.appendChild(confronto);
+                confronto.classList.add("confronto")
+                const mandante = document.createElement("div")
+                confronto.appendChild(mandante);
+                mandante.classList.add("time-mandante")
+                const nomeMandante = document.createElement("span")
+                mandante.appendChild(nomeMandante)
+                nomeMandante.classList.add("nome-time")
+                nomeMandante.textContent = jogo.mandante.nome
+
+                const logoMandante = document.createElement("span")
+                mandante.appendChild(logoMandante)
+
+                const imgMandante = document.createElement("img")
+                logoMandante.appendChild(imgMandante)
+                imgMandante.classList.add("logo-escudo")
+                imgMandante.src = jogo.mandante.escudo
+                imgMandante.alt = jogo.mandante.nome
+
+                const versus = document.createElement("span")
+                confronto.appendChild(versus);
+                versus.classList.add("versus")
+                versus.textContent = "X"
+
+                const visitante = document.createElement("div")
+                confronto.appendChild(visitante);
+                visitante.classList.add("time-visitante")
+
+                const logoVisitante = document.createElement("span")
+                visitante.appendChild(logoVisitante)
+
+                const imgVisitante = document.createElement("img")
+                logoVisitante.appendChild(imgVisitante)
+                imgVisitante.classList.add("logo-escudo")
+                imgVisitante.src = jogo.visitante.escudo
+                imgVisitante.alt = jogo.visitante.nome
+
+                const nomeVisitante = document.createElement("span")
+                visitante.appendChild(nomeVisitante)
+                nomeVisitante.classList.add("nome-time")
+                nomeVisitante.textContent = jogo.visitante.nome
+
+                const transmissao = document.createElement("ul")
+                divJogo.appendChild(transmissao);
+                transmissao.classList.add("transmissao")
+                for (var i = 0; i < jogo.transmissao.length; i++) {
+                    const canal = document.createElement("li")
+                    transmissao.appendChild(canal);
+                    const linkCanal = document.createElement("a")
+                    canal.appendChild(linkCanal)
+                    linkCanal.href = jogo.transmissao[i].link
+                    const logoCanal = document.createElement("img")
+                    linkCanal.appendChild(logoCanal)
+                    logoCanal.classList.add("logo-transmissao")
+                    logoCanal.src = jogo.transmissao[i].logoCanal
+                    logoCanal.alt = jogo.transmissao[i].nomeCanal
+                }
+            })
+        })
+}
+
+jogoAmanha();
