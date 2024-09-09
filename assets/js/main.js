77d09b02ecd0a82fb2d541b585b00978ee5530ed
JV Freitas <134442764+jvfreitas61@users.noreply.github.com>
2024-09-09 19:31:19 -0300

    ATUALIZACAO-JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,8 +23,6 @@ btnAvancar.addEventListener("click", function () {
     titulos[listaAtual].classList.add("titulo-selecionado");
 });
 
-
-
 btnVoltar.addEventListener("click", function () {
     if (listaAtual === 0) return;
     //const listaSelecionado = document.querySelector(".selecionado");
@@ -42,30 +40,14 @@ function removerLista() {
     tituloSelecionado.classList.remove("titulo-selecionado");
 }
 
-/*var jogo = document.querySelectorAll('.jogo');
-jogo.addEventListener('click', adicionaClasse());
-
-function adicionaClasse(){
-    jogo.classList.add('selecionado');
-}
-
-function abrirJogo(){
-
-}*/
-
 const arrayJogos = document.querySelectorAll('.jogo');
 const jogoAlt = document.querySelector('#jogo-alt')
-/*console.log(arrayJogos);*/
+
 
 arrayJogos.forEach((jogo) => {
     jogo.addEventListener('click', callBack)
 
 })
-
-
-
-
-
 
 function callBack(event) {
 
@@ -154,29 +136,22 @@ function fecharJogo() {
     jogoAlt.classList.remove("jogo-fechado")
 }
 
-//const tituloJogo = document.querySelector('.estadio');
-//console.log(tituloJogo);
+
+const jogosHoje = fetch('assets/json/jogos1.json');
+const listaJogosHoje = document.querySelector('#jogosHoje');
+criarJogo(jogosHoje, listaJogosHoje);
+const jogosAmanha = fetch('assets/json/jogos2.json');
+const listaJogosAmanha = document.querySelector('#jogosAmanha');
+criarJogo(jogosAmanha, listaJogosAmanha);
+const bra28 = fetch('assets/json/brasileirao/rodada28.json');
+const listaBra28 = document.querySelector('#braRodada28');
+criarJogo(bra28, listaBra28);
 
 
-/*
-const jogos = document.querySelector('.listaJogos')
-jogos.addEventListener('click', callbackLista);
-
-function callbackLista(event){
-    console.log(event.target);
-}*/
-
-//
-//const btnFecharJogo = document.querySelector('#fechar-jogo');
-//const jogoAlt = document.getElementById('jogo-alt');
-
-
-
-function jogoHoje() {
-    fetch('assets/js/jogosHoje.json')
+function criarJogo(jogosJson, listaJogos) {
+    jogosJson
         .then(response => response.json())
         .then(jogos => {
-            const listaJogos = document.querySelector('#jogosHoje');
             //console.log(listaJogos);
             jogos.map(jogo => {
                 const card = document.createElement("li");
@@ -271,104 +246,6 @@ function jogoHoje() {
             })
         })
 }
-
-jogoHoje();
-
-
-
-function jogoAmanha() {
-    fetch('assets/js/jogosAmanha.json')
-        .then(response => response.json())
-        .then(jogos => {
-            const listaJogos = document.querySelector('#jogosAmanha');
-            //console.log(listaJogos);
-            jogos.map(jogo => {
-                const card = document.createElement("li");
-                listaJogos.appendChild(card);
-                const divJogo = document.createElement("div");
-                card.appendChild(divJogo);
-                divJogo.classList.add("jogo");
-                const jogoHeading = document.createElement("div")
-                divJogo.appendChild(jogoHeading);
-                jogoHeading.classList.add("jogo-heading");
-                const dataJogo = document.createElement("p")
-                jogoHeading.appendChild(dataJogo);
-                dataJogo.classList.add("data-jogo")
-                dataJogo.textContent = jogo.dataJogo
-
-                const estadio = document.createElement("p")
-                jogoHeading.appendChild(estadio);
-                estadio.classList.add("estadio")
-                estadio.textContent = jogo.estadio
-
-                const horaJogo = document.createElement("p")
-                jogoHeading.appendChild(horaJogo);
-                horaJogo.classList.add("horario-jogo")
-                horaJogo.textContent = jogo.horaJogo
-
-                const confronto = document.createElement("div");
-                divJogo.appendChild(confronto);
-                confronto.classList.add("confronto")
-                const mandante = document.createElement("div")
-                confronto.appendChild(mandante);
-                mandante.classList.add("time-mandante")
-                const nomeMandante = document.createElement("span")
-                mandante.appendChild(nomeMandante)
-                nomeMandante.classList.add("nome-time")
-                nomeMandante.textContent = jogo.mandante.nome
-
-                const logoMandante = document.createElement("span")
-                mandante.appendChild(logoMandante)
-
-                const imgMandante = document.createElement("img")
-                logoMandante.appendChild(imgMandante)
-                imgMandante.classList.add("logo-escudo")
-                imgMandante.src = jogo.mandante.escudo
-                imgMandante.alt = jogo.mandante.nome
-
-                const versus = document.createElement("span")
-                confronto.appendChild(versus);
-                versus.classList.add("versus")
-                versus.textContent = "X"
-
-                const visitante = document.createElement("div")
-                confronto.appendChild(visitante);
-                visitante.classList.add("time-visitante")
-
-                const logoVisitante = document.createElement("span")
-                visitante.appendChild(logoVisitante)
-
-                const imgVisitante = document.createElement("img")
-                logoVisitante.appendChild(imgVisitante)
-                imgVisitante.classList.add("logo-escudo")
-                imgVisitante.src = jogo.visitante.escudo
-                imgVisitante.alt = jogo.visitante.nome
-
-                const nomeVisitante = document.createElement("span")
-                visitante.appendChild(nomeVisitante)
-                nomeVisitante.classList.add("nome-time")
-                nomeVisitante.textContent = jogo.visitante.nome
-
-                const transmissao = document.createElement("ul")
-                divJogo.appendChild(transmissao);
-                transmissao.classList.add("transmissao")
-                for (var i = 0; i < jogo.transmissao.length; i++) {
-                    const canal = document.createElement("li")
-                    transmissao.appendChild(canal);
-                    const linkCanal = document.createElement("a")
-                    canal.appendChild(linkCanal)
-                    linkCanal.href = jogo.transmissao[i].link
-                    const logoCanal = document.createElement("img")
-                    linkCanal.appendChild(logoCanal)
-                    logoCanal.classList.add("logo-transmissao")
-                    logoCanal.src = jogo.transmissao[i].logoCanal
-                    logoCanal.alt = jogo.transmissao[i].nomeCanal
-                }
-            })
-        })
-}
-
-jogoAmanha();
 
 const linksExternos = document.querySelectorAll('a[href^="https"]');
 linksExternos.forEach((link) => {
